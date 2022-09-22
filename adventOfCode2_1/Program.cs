ae113a72f794b3c0fce0d655e739d1f11fd54115
Vitamin <86688525+VitaminA-plays@users.noreply.github.com>
2022-09-22 14:13:29 -0400

    Add files via upload

---
+++ adventOfCode2_1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+
+string[] instructions = File.ReadAllLines(@"C:\Users\Axion\OneDrive\Desktop\AdventOfCode\instructions.txt");
+
+int horizontal = 0;
+int depth = 0;
+
+foreach (string line in instructions)
+{
+    if (line.Contains("forward"))
+    {
+        horizontal += int.Parse(GetNumbers(line));
+    }
+    if (line.Contains("down"))
+    {
+        depth += int.Parse(GetNumbers(line));
+    }
+    if (line.Contains("up"))
+    {
+        depth -= int.Parse(GetNumbers(line));
+    }
+}
+
+int mult = horizontal * depth;
+
+Console.WriteLine(mult);
+
+static string GetNumbers(string input)
+{
+    return new string(input.Where(c => char.IsDigit(c)).ToArray());
+}
+
